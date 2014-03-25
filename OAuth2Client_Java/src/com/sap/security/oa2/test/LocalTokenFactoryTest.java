9fef3d11a19c39e0f9d71b3723ec629e59436755
DEBOER <martijn.de.boer@sap.com>
2014-03-25 17:31:55 +0100

    Add SAML2 metadata

--- OAuth2Client_Java/src/com/sap/security/oa2/test/LocalTokenFactoryTest.java
+++ OAuth2Client_Java/src/com/sap/security/oa2/test/LocalTokenFactoryTest.java
@@ -3,12 +3,9 @@ package com.sap.security.oa2.test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
@@ -20,14 +17,12 @@ import javax.net.ssl.X509TrustManager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runners.JUnit4;
 
 import com.sap.security.oa2.LocalSamlTokenFactory;
 import com.sap.security.oa2.OAuth2SAML2AccessToken;
+import com.sap.security.oa2.TrustData;
 import com.sap.security.oa2.trace.OAuthTraceData;
 import com.sap.security.oa2.trace.OAuthTracer;
-
-import static org.junit.Assert.*;
 
 public class LocalTokenFactoryTest {
 
@@ -66,6 +61,13 @@ public class LocalTokenFactoryTest {
     };
 
     @Test
+    public void dumpSAML2Metadata() throws Exception {
+	System.out.println("SAML2 Metadata");
+	new TrustData(configurationProperties).createMetadata(System.out);
+	System.out.println();
+    }
+
+    @Test
     public void testGetAT2() throws Exception {
 	try {
 	    OAuth2SAML2AccessToken atf = new OAuth2SAML2AccessToken(f, configurationProperties);
@@ -74,20 +76,10 @@ public class LocalTokenFactoryTest {
 	    Assert.assertTrue(at != null);
 	} catch (Exception ex) {
 	    ex.printStackTrace();
+	    dumpTraces();
 	    Assert.assertFalse("no access token received", true);
 	}
-	List<OAuthTraceData> traceDataList = OAuthTracer.getTraceData();
-	for (OAuthTraceData td : traceDataList) {
-	    if (td.getType() == OAuthTracer.TEXT_TYPE)
-		System.out.println(td.getDescription() + ":" + td.getDataText());
-	    if (td.getType() == OAuthTracer.XML_TYPE)
-		System.out.println(td.getDescription() + ":" + new String(td.getData()));
-	    if (td.getType() == OAuthTracer.HTTP_TYPE)
-		System.out.println(td.getDescription() + ":" + new String(td.getData()));
-
-	    System.out.println();
-
-	}
+	dumpTraces();
 
 	// usage of an access token
 	/*
@@ -104,6 +96,22 @@ public class LocalTokenFactoryTest {
 	 * is.read()) != -1) { bos.write(dataElement); } byte[] inData =
 	 * bos.toByteArray(); System.out.println(bos);
 	 */
+    }
+
+    /**
+     * 
+     */
+    private void dumpTraces() {
+	List<OAuthTraceData> traceDataList = OAuthTracer.getTraceData();
+	for (OAuthTraceData td : traceDataList) {
+	    if (td.getType() == OAuthTracer.TEXT_TYPE)
+		System.out.println(td.getDescription() + ":" + td.getDataText());
+	    if (td.getType() == OAuthTracer.XML_TYPE)
+		System.out.println(td.getDescription() + ":" + new String(td.getData()));
+	    if (td.getType() == OAuthTracer.HTTP_TYPE)
+		System.out.println(td.getDescription() + ":" + new String(td.getData()));
+	    System.out.println();
+	}
     }
 
     private byte[] readData(InputStream is) throws IOException {
